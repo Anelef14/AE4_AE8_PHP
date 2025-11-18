4af11e80afb8fa49699d28589ea57a4c60f500ca
Anastasia <anelef14@gmail.com>
2025-11-18 22:00:25 +0100

    step 3: basic validation and error page with back button

--- confirm.php
+++ confirm.php
@@ -9,3 +9,36 @@ $date = isset($_POST['date']) ? $_POST['date'] : '';
 $people = isset($_POST['people']) ? $_POST['people'] : ''; 
 $email = isset($_POST['email']) ? $_POST['email'] : ''; 
 $name = isset($_POST['name']) ? $_POST['name'] : '';
+
+$errors = [];
+
+if ($restaurant === '') $errors[] = "Restaurant is required."; 
+if ($date === '') $errors[] = "Date is required."; 
+if ($people === '') $errors[] = "Number of people is required."; 
+if ($email === '') $errors[] = "Email is required."; 
+if ($name === '') $errors[] = "Customer name is required."; 
+
+if (!empty($errors)) { 
+    ?> 
+    <!DOCTYPE html> 
+    <html lang="en"> 
+        <head> 
+            <meta charset="UTF-8"> 
+            <title>Form Error</title> 
+        </head> 
+        <body> 
+            <h1>Form Error</h1> 
+            <p>Please correct these errors:</p> 
+            <ul> 
+                <?php foreach ($errors as $error): ?> 
+                    <li><?php echo $error; ?></li> 
+                    <?php endforeach; ?> 
+                </ul> 
+                
+                <form action="reservation.php" method="get"> 
+                    <input type="hidden" name="restaurant" value="<?php echo $restaurant; ?>"> 
+                    <button type="submit">Back to form</button> 
+                </form> 
+            </body> 
+            </html> 
+            <?php exit; } ?>
