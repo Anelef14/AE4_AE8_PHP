0ddacca73be0fcfabe045534ccdcb4af14c54bad
Anastasia <anelef14@gmail.com>
2025-11-18 21:28:10 +0100

    Step 2: read POST data variables

--- confirm.php
+++ confirm.php
@@ -3,3 +3,9 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     header('Location: reservation.php');
     exit;
 }
+
+$restaurant = isset($_POST['restaurant']) ? $_POST['restaurant'] : ''; 
+$date = isset($_POST['date']) ? $_POST['date'] : ''; 
+$people = isset($_POST['people']) ? $_POST['people'] : ''; 
+$email = isset($_POST['email']) ? $_POST['email'] : ''; 
+$name = isset($_POST['name']) ? $_POST['name'] : '';
